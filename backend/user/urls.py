ded56eae82e329a355762c21f5e474dbc4f2d180
1019104-Ryo-Matsumura <matsumuraryo0827@gmail.com>
2024-09-08 16:01:10 +0900

    マージ用にファイル編集

--- backend/user/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import RegisterView, LoginView, ProtectedView
-
-urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('protected/', ProtectedView.as_view(), name="token")
-]
