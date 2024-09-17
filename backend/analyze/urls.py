d16f3020148a06e82357b34f5224415382fa09dd
1019104-Ryo-Matsumura <matsumuraryo0827@gmail.com>
2024-09-18 07:36:17 +0900

    マージ用ファイル修正

--- backend/analyze/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import record_mistype_api, analyze_mistypes_api
-
-urlpatterns = [
-    path('record_mistype/', record_mistype_api, name='record_mistype_api'),
-    path('analyze_mistypes/', analyze_mistypes_api, name='analyze_mistypes_api'),
-]
