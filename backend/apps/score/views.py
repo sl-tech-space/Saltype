31211e1151e3d1208c9b301980ef6819bf4f1f7a
1019104-Ryo-Matsumura <matsumuraryo0827@gmail.com>
2024-10-12 23:29:55 +0900

    リロードした際スコアがインサートされてしまう問題を解決

--- backend/apps/score/views.py
+++ backend/apps/score/views.py
@@ -2,6 +2,7 @@ import logging
 
 from apps.common.models import Score
 from apps.common.utils import HandleExceptions
+from django.core.cache import cache
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -38,30 +39,33 @@ class ScoreAndRankHandler(APIView):
             """平均スコア取得"""
             average_score = score_service.get_average_score(user_id, lang_id, diff_id)
             """スコア計算"""
-            typing_count = request.data.get('typing_count', 0)
-            accuracy = request.data.get('accuracy', 1.0)
-            score = score_service.calculate_score(typing_count, accuracy)
-            """score_dataからscoreフィールドを削除"""
-            score_data.pop('score', None)
-            """スコアを保存"""
-            score_instance = Score.objects.create(**score_data, score=score)
-            """最高スコア判定"""
-            is_high_score, new_highest_score = score_service.is_new_high_score(score)
-            """ランク更新処理"""
-            rank = score_service.update_rank(score, is_high_score)
-            """ランキング取得"""
-            ranking_position = score_service.get_ranking_position(score)
+            typing_count = request.data.get('typing_count')
+            accuracy = request.data.get('accuracy')
+            if typing_count and accuracy:
+                score = score_service.calculate_score(typing_count, accuracy)
+                """score_dataからscoreフィールドを削除"""
+                score_data.pop('score', None)
+                """スコアを保存"""
+                score_instance = Score.objects.create(**score_data, score=score)
+                """最高スコア判定"""
+                is_high_score, new_highest_score = score_service.is_new_high_score(score)
+                """ランク更新処理"""
+                rank = score_service.update_rank(score, is_high_score)
+                """ランキング取得"""
+                ranking_position = score_service.get_ranking_position(score)
 
-            return Response(
-                {
-                    'score': score,
-                    'average_score': average_score,
-                    'is_high_score': is_high_score,
-                    'highest_score': new_highest_score,
-                    'rank': rank,
-                    'ranking_position': ranking_position,
-                },
-                status=status.HTTP_200_OK)
+                return Response(
+                    {
+                        'score': score,
+                        'average_score': average_score,
+                        'is_high_score': is_high_score,
+                        'highest_score': new_highest_score,
+                        'rank': rank,
+                        'ranking_position': ranking_position,
+                    },
+                    status=status.HTTP_200_OK)
+
+        return Response({'message': 'スコアインサートスキップ'}, status=status.HTTP_200_OK)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
