1d1c7de17030a2eaf97782c1776d6603f3b4f13b
Memolia <ultimum.memolia@gmail.com>
2024-10-20 20:14:09 +0900

    不要なファイルを削除

--- sql/create/permission.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE permission (
-    permission_id INT AUTO_INCREMENT PRIMARY KEY,
-    permission_name VARCHAR(255) NOT NULL,
-    description TEXT
-);
--- sql/insert/permission.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO
-    permission (permission_name, description)
-VALUES
-    ('ADMIN', '管理者権限。システム全体の管理が可能。'),
-    ('USER', '一般利用者権限。通常の利用が可能。');
