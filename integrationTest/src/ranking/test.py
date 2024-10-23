bf2a15baf35dd1a46143f99dbb2f261b047756fd
Memolia <ultimum.memolia@gmail.com>
2024-10-23 20:49:32 +0900

    dockerを全パターンとDBのみパターンに切り替えれるように。IT完了

--- integrationTest/src/ranking/test.py
+++ integrationTest/src/ranking/test.py
@@ -4,52 +4,52 @@ from selenium.webdriver.common.by import By # type: ignore
 from selenium.webdriver.support.ui import WebDriverWait # type: ignore
 from selenium.webdriver.support import expected_conditions as EC # type: ignore
 
-""" python -m pytest src/analyze/test.py """
-class TestAnalyzePage:
+""" python -m pytest src/ranking/test.py """
+class TestRankingPage:
     
-    """ 項番1. 分析情報画面が表示されること """
-    def test_analzye1(self):
+    """ 項番1. ランキング画面が表示されること """
+    def test_ranking1(self):
         driver = SeleniumUtil.initialize_driver()
         
         SeleniumUtil.open_page(driver, 'http://localhost:3000')
         
         try:
-            TestAnalyzePage.login(driver)
+            TestRankingPage.login(driver)
             
-            analzye_element = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, "//ul[@class='menu-list']/li[2]/p"))
+            ranking_element = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, "//ul[@class='menu-list']/li[1]/p"))
             )
             
-            analzye_element.click()
+            ranking_element.click()
             
-            WebDriverWait(driver, 10).until(EC.title_contains("分析情報"))
+            WebDriverWait(driver, 10).until(EC.title_contains("ランキング"))
             
-            SeleniumUtil.take_screenshot(driver, "analzye", "項番1", "分析情報画面")
+            SeleniumUtil.take_screenshot(driver, "ranking", "項番1", "ランキング画面")
         
         except Exception as e:
-            print(f"項番1. 分析情報画面が表示されること : {e}")
+            print(f"項番1. ランキング画面が表示されること : {e}")
         
         finally:
             SeleniumUtil.close_driver(driver)
 
     """ 項番2. 戻るボタンでホームに遷移すること """
-    def test_analzye2(self):
+    def test_ranking2(self):
         driver = SeleniumUtil.initialize_driver()
         
         SeleniumUtil.open_page(driver, 'http://localhost:3000')
         
         try:
-            TestAnalyzePage.login(driver)
+            TestRankingPage.login(driver)
             
-            analzye_element = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, "//ul[@class='menu-list']/li[2]/p"))
+            ranking_element = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, "//ul[@class='menu-list']/li[1]/p"))
             )
             
-            analzye_element.click()
+            ranking_element.click()
             
-            WebDriverWait(driver, 10).until(EC.title_contains("分析情報"))
+            WebDriverWait(driver, 10).until(EC.title_contains("ランキング"))
             
-            SeleniumUtil.take_screenshot(driver, "analzye", "項番2", "分析情報画面")
+            SeleniumUtil.take_screenshot(driver, "ranking", "項番2", "ランキング画面")
             
             back_button = WebDriverWait(driver, 10).until(
                 EC.element_to_be_clickable((By.CSS_SELECTOR, ".header-right button"))
@@ -59,7 +59,7 @@ class TestAnalyzePage:
             
             WebDriverWait(driver, 10).until(EC.title_contains("ホーム"))
             
-            SeleniumUtil.take_screenshot(driver, "analzye", "項番2", "戻るボタン押下後")
+            SeleniumUtil.take_screenshot(driver, "ranking", "項番2", "戻るボタン押下後")
         
         except Exception as e:
             print(f"項番2. 戻るボタンでホームに遷移すること : {e}")
