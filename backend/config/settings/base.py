5d9f93013ebbee7d5de1cdefa812257d0ff5b498
Memolia <ultimum.memolia@gmail.com>
2024-10-24 19:28:07 +0900

    本番環境用ディレクトリ作成。ログインまで可能

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -85,7 +85,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000']
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost']
 
 CORS_ALLOW_HEADERS = [
     'authorization',
