bec82fa254157fd9213c27f4e67c6b52ee0f92c3
ryo.matsumura <matsumuraryo0827@mail.com>
2025-01-02 13:04:21 +0900

    シリアライザ処理統一

--- backend/apps/mistype/serializers.py
+++ backend/apps/mistype/serializers.py
@@ -3,16 +3,31 @@ from apps.common.models import User
 from apps.common.serializers import BaseSerializer
 from rest_framework.exceptions import ValidationError
 
+
 class MistypeSerializer(BaseSerializer):
     user_id = serializers.UUIDField()
-    mistypes = serializers.ListField(child=serializers.DictField(), required=False, allow_empty=True)
+    mistypes = serializers.ListField(
+        child=serializers.DictField(), required=False, allow_empty=True
+    )
     limit = serializers.IntegerField(required=False)
 
     def validate(self, attrs):
-        self.validate_existence(attrs, "user_id", User, "指定されたユーザーは存在しません。")
+        # ユーザーの存在を確認し、オブジェクトを追加
+        user_id = attrs.get("user_id")
+        if user_id:
+            try:
+                attrs["user"] = User.objects.get(pk=user_id)
+            except User.DoesNotExist:
+                raise ValidationError({"user_id": "指定されたユーザーは存在しません。"})
+
         mistypes = attrs.get("mistypes", [])
-        if any(not isinstance(item.get("miss_count"), int) or item.get("miss_count") < 0 for item in mistypes):
-            raise ValidationError("ミスタイプのカウントは正の整数でなければなりません。")
+        for item in mistypes:
+            miss_char = item.get("miss_char")
+            if not miss_char.isalpha():
+                raise ValidationError("miss_charはアルファベットでなければなりません。")
+            if not isinstance(item.get("miss_count"), int) or item.get("miss_count") < 0:
+                raise ValidationError("miss_countは正の整数でなければなりません。")
+
         limit = attrs.get("limit")
         if limit is not None and limit <= 0:
             raise ValidationError("limitは正の整数である必要があります。")
