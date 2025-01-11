3e1c242efd709c14992fbf59d79aa61b64be2359
ryo.matsumura <matsumuraryo0827@mail.com>
2025-01-11 16:17:56 +0900

    認証ビューとコンタクト・ビューをリファクタリングし、リクエスト処理を効率化。BaseAuthViewの名前をBaseAuthenticationViewに変更し、新しいpostメソッドを利用するために関連するビューを更新しました。未使用のhandle_post_requestメソッドとhandle_get_requestメソッドを削除し、わかりやすくしました。メール送信をContactViewに直接統合し、ContactEmailクラスを廃止。全体的なコード構造と保守性を改善。

--- backend/apps/contact/views.py
+++ backend/apps/contact/views.py
@@ -1,4 +1,7 @@
-from apps.contact.email import ContactEmail
+from django.conf import settings
+from django.core.mail import send_mail
+from django.contrib.auth import get_user_model
+from rest_framework.exceptions import ValidationError
 from .base_view import BaseContactView
 
 
@@ -18,10 +21,23 @@ class ContactView(BaseContactView):
         Returns:
             dict: 要望送信結果を含むレスポンスデータ。
         """
-        # ContactEmailクラスを使用して要望をメールで送信
-        request_email = ContactEmail(
-            validated_data["user_id"], validated_data["request_content"]
+        user_id = validated_data["user_id"]
+        request_content = validated_data["request_content"]
+
+        # ユーザー情報を取得
+        User = get_user_model()
+        user = User.objects.get(pk=user_id)
+
+        # メール送信
+        subject = f"{user.username}から要望が届いています。"
+        message = f"{user.username} から:\n\n{request_content}\n"
+
+        send_mail(
+            subject=subject,
+            message=message,
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[settings.EMAIL_HOST_USER],
+            fail_silently=False,
         )
-        request_email.send_request_email()
 
         return {"message": "要望が正常に送信されました。"}
