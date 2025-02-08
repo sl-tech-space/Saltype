c1b64c6d6466709fa39afef48f01164b5f3683ba
Memolia <ultimum.memolia@gmail.com>
2025-02-08 19:36:44 +0900

    カルーセル難易度保存作成

---
+++ frontend/pages/settings/screen.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+onMounted(() => {
+    useHead({
+        title: "画面設定 | Saltype"
+    });
+});
+</script>
+
+<template>
+    <div class="page">
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.page {
+    @extend %full-page;
+}
+</style>
