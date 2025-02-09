8f54a2c44e815c88c59f6f696b273609ff46e2d9
ryo.matsumura <matsumuraryo0827@mail.com>
2025-02-09 20:23:23 +0900

    クーロンの変更

--- backend/apps/score/management/commands/send_daily_scores.py
+++ backend/apps/score/management/commands/send_daily_scores.py
@@ -9,12 +9,14 @@ class Command(BaseCommand):
     help = "本日のベストスコアをメールで送信する"
 
     def handle(self, *args, **options):
-        # メールアドレスを設定から取得
-        recipient_emails = settings.RECIPIENT_EMAILS
-        sender_email = settings.SENDER_EMAIL
-
-        # メールアドレスに対応するユーザーを取得
-        users = User.objects.filter(email__in=recipient_emails)
+        # スコアを取得するユーザーのメールアドレス
+        get_score_user_emails = settings.GET_SCORES_EMAILS
+        # 実際にスコアを送信する相手のメールアドレス
+        to_sending_emails = settings.TO_SEND_EMAILS
+        # メール送信元のメールアドレス
+        host_email = settings.EMAIL_HOST_USER
+        # スコアを取得するユーザーのメールアドレスリストから、ユーザーを取得
+        users = User.objects.filter(email__in=get_score_user_emails)
 
         # 各ユーザーの本日のベストスコアを取得
         scores_list = []
@@ -26,15 +28,21 @@ class Command(BaseCommand):
         # スコアを高い順にソート
         scores_list.sort(key=lambda x: x[1], reverse=True)
 
+        # 日付を取得してフォーマット
+        today = date.today()
+        today_str = today.strftime('%m/%d')
+
+        # メールの件名に日付を追加
+        subject = f"{today_str} - 新卒タイピングスコアランキング"
+
         # メールの内容を作成
-        subject = "本日のスコアランキング！"
-        message_lines = ["本日のスコアランキング！\n"]
+        message_lines = [f"{today_str} のスコアランキングを送信します。\n"]
         for rank, (username, score) in enumerate(scores_list, start=1):
-            message_lines.append(f"{rank}位: {username} - スコア: {score}")
+            message_lines.append(f"{rank}位 : {username} - スコア : {score}")
         message = "\n".join(message_lines)
 
         # メールを送信
-        send_mail(subject, message, sender_email, recipient_emails)
+        send_mail(subject, message, host_email, to_sending_emails)
 
     def get_today_highest_score(self, user):
         today = date.today()
