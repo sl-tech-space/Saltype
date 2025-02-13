9480fc694807e7ffd79fc71b38c97201604dbd69
ryo.matsumura <matsumuraryo0827@mail.com>
2025-02-13 22:31:07 +0900

    パスワード変更機能実装

--- backend/apps/user/urls.py
+++ backend/apps/user/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 
-from .views import GetUsersView, GetUserView, UpdateUserView, DeleteUserView
+from .views import (
+    GetUsersView,
+    GetUserView,
+    UpdateUserView,
+    DeleteUserView,
+    PasswordResetView,
+)
 
 urlpatterns = [
     path("", GetUsersView.as_view(), name="get_users"),
     path("<uuid:user_id>/", GetUserView.as_view(), name="get_user"),
     path("update/", UpdateUserView.as_view(), name="update_user"),
     path("delete/", DeleteUserView.as_view(), name="delete_user"),
+    path("password_reset/", PasswordResetView.as_view(), name="password_reset"),
+    path('password_reset/<str:token>/', PasswordResetView.as_view(), name='password_reset'),  # トークン付きURL
 ]
