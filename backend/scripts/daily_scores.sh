950f6ab0bebfab73c97835834db5962bd36fe3aa
ryo.matsumura <matsumuraryo0827@mail.com>
2025-02-18 22:53:20 +0900

    最高スコアメール送信機能修正

--- backend/scripts/daily_scores.sh
+++ backend/scripts/daily_scores.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-echo "Starting the script" >> /var/log/cron.log
+
+log() {
+    local level="$1"
+    shift
+    echo "$(date '+%Y-%m-%d %H:%M:%S') [$level] $*" >> /var/log/cron.log
+}
+
+log "INFO" "Starting the script"
 cd /app
-/usr/local/bin/python manage.py send_daily_scores >> /var/log/cron.log 2>&1
-echo "Script finished" >> /var/log/cron.log
+if /usr/local/bin/python manage.py send_daily_scores >> /var/log/cron.log 2>&1; then
+    log "INFO" "Script finished successfully"
+else
+    log "ERROR" "Script encountered an error"
+fi
