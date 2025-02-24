17f5d50fad3cfe32cad411c13bd279f10ca2e79c
ryo.matsumura <matsumuraryo0827@mail.com>
2025-02-24 14:16:04 +0900

    シリアライザーの修正

--- backend/apps/authentication/serializers.py
+++ backend/apps/authentication/serializers.py
@@ -11,58 +11,30 @@ password_validator = RegexValidator(
     message="パスワードは大文字英字、数字、記号をそれぞれ1文字以上含む必要があります。",
 )
 
-
-class AuthenticationSerializer(BaseSerializer):
+class LoginSerializer(BaseSerializer):
     """
-    ユーザー認証に関連するリクエストデータを検証するシリアライザクラス
+    ユーザーがオリジナルフォームからログインするためのシリアライザー。
     """
-
     email = serializers.EmailField(max_length=256, required=True)  # メールアドレス
     password = serializers.CharField(
         write_only=True,
-        required=False,
+        required=True,
         style={"input_type": "password"},
         min_length=8,
         max_length=100,
         validators=[password_validator],
     )  # パスワード
-    username = serializers.CharField(max_length=15, required=False)  # ユーザー名
-    picture = serializers.URLField(required=False, allow_blank=True)  # 画像URL
 
     def validate(self, attrs):
         """
         リクエストデータに対してバリデーションを実行します。
-
-        Args:
-            attrs (dict): バリデーション対象のデータ。
-        Returns:
-            attrs: バリデーションを通過したデータ。
         """
-        # メールアドレスのバリデーション
-        attrs = self.check_email(attrs)
-
-        # パスワードのバリデーション
-        password = attrs.get("password")
-        if password:
-            attrs["user"] = self.check_user(attrs.get("email"), attrs.get("password"))
-        else:
-            attrs = self.check_username(attrs)
-
+        attrs["user"] = self.check_user(attrs.get("email"), attrs.get("password"))
         return attrs
 
     def check_user(self, email, password):
         """
         ユーザーのバリデーションを行います。
-
-        Args:
-            email (str): メールアドレス。
-            password (str): パスワード。
-
-        Returns:
-            user: 認証されたユーザー。
-
-        Raises:
-            ValidationError: 認証に失敗した場合。
         """
         user = authenticate(
             request=self.context.get("request"), username=email, password=password
@@ -78,3 +50,19 @@ class AuthenticationSerializer(BaseSerializer):
                 code="authorization",
             )
         return user
+
+
+class GoogleAuthSerializer(BaseSerializer):
+    """
+    ユーザーがGoogle認証を行うためのシリアライザー。
+    """
+    email = serializers.EmailField(max_length=256, required=True)  # メールアドレス
+    username = serializers.CharField(max_length=15, required=False)  # ユーザー名
+
+    def validate(self, attrs):
+        """
+        リクエストデータに対してバリデーションを実行します。
+        """
+        attrs = self.check_email(attrs)
+        attrs = self.check_username(attrs)
+        return attrs
