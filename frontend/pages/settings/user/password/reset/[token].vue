9f0b1dc1f5024ad4607f610ec71d8781366e2b39
Memolia <ultimum.memolia@gmail.com>
2025-03-01 10:57:07 +0900

    パスワード再設定機能作成

--- frontend/pages/settings/user/password/reset/[token].vue
+++ frontend/pages/settings/user/password/reset/[token].vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
+import AuthHeader from '~/components/organisms/auth/AuthHeader.vue';
+import Reset from '~/components/organisms/settings/user/password/reset/Reset.vue';
 import Loading from '~/components/molecules/common/ui/Loading.vue';
 import { usePasswordReset } from '~/composables/settings/usePasswordReset';
 
 const route = useRoute();
-const token = route.params.token;
+const token = computed(() => {
+  const routeToken = route.params.token;
+  return Array.isArray(routeToken) ? routeToken[0] : routeToken;
+});
 const { tokenIsValid, isLoading } = usePasswordReset();
 
 onMounted(async () => {
-    const isValid = await tokenIsValid(token as string);
+    const isValid = await tokenIsValid(token.value);
     if (!isValid) {
         await navigateTo({ name: "login" });
     }
@@ -15,7 +20,17 @@ onMounted(async () => {
 </script>
 
 <template>
-    <div>
+    <div class="page">
+        <AuthHeader />
+        <Reset :token="token" />
         <Loading :is-loading="isLoading" />
     </div>
 </template>
+
+<style lang="scss" scoped>
+.page {
+  height: 100vh;
+  @extend %full-page;
+  @include vertical-centered-flex;
+}
+</style>
