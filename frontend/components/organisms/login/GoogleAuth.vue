161ce4fdb829b934ebacc4e659507c7ca3d1b798
Memolia <ultimum.memolia@gmail.com>
2025-03-08 09:42:22 +0900

    Google認証タブを開いている間ローディングを適用する

--- frontend/components/organisms/login/GoogleAuth.vue
+++ frontend/components/organisms/login/GoogleAuth.vue
@@ -3,7 +3,6 @@ import Separator from "~/components/atoms/ui/Separator.vue";
 import Text from "~/components/atoms/texts/Text.vue";
 import Button from "~/components/atoms/buttons/Button.vue";
 import Image from "~/components/atoms/imgs/Image.vue";
-import Loading from "~/components/molecules/common/ui/Loading.vue";
 import BaseNotification from "~/components/molecules/common/BaseNotification.vue";
 import GoogleIcon from "~/assets/images/login/web_dark_rd_na@2x.png";
 import { useGoogleAuth } from "~/composables/auth/useGoogleAuth";
@@ -12,7 +11,7 @@ const googleIcon = ref(GoogleIcon);
 const config = useRuntimeConfig();
 const showErrorNotification = ref(false);
 
-const { loginWithGoogle, isLoading, error } = useGoogleAuth();
+const { loginWithGoogle, error } = useGoogleAuth();
 const handleLoginWithGoogle = async (): Promise<void> => {
   await loginWithGoogle();
 }
@@ -53,7 +52,6 @@ onMounted(() => {
       </template>
     </Button>
   </div>
-  <Loading :is-loading="isLoading" />
   <BaseNotification v-if="error" message="エラーが発生しました" :content="error" :show="showErrorNotification" />
 </template>
 
