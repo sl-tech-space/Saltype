6272533e520638728da4da34615ab90e1d6c3a8c
root <root@Saltype.tpmo14mdummexixdn3si4hhjnc.lx.internal.cloudapp.net>
2025-04-05 00:04:19 +0900

    ランキング仕様修正

--- backend/apps/ranking/views.py
+++ backend/apps/ranking/views.py
@@ -2,21 +2,18 @@ from apps.common.models import Score
 from apps.common.views import BaseView
 from datetime import date
 from .serializers import GetRankingSerializer
-
-
+from django.db.models import Max, Window, F
+from django.db.models.functions import Rank
 class GetRankingView(BaseView):
     """
     ランキング情報を取得するためのAPIビュークラス。
     通常のランキングと日別ランキングを取得する機能を提供します。
     """
-
     def post(self, request, *args, **kwargs):
         return super().post(request, GetRankingSerializer, *args, **kwargs)
-
     def handle_post_request(self, validated_data: dict) -> dict:
         """
         リクエストデータに基づいてランキングデータを取得します。
-
         Args:
             validated_data (dict): 検証済みのリクエストデータ。
                 - date (date, optional): 日別ランキング取得時の日付
@@ -35,29 +32,24 @@ class GetRankingView(BaseView):
         lang_id = validated_data.get("lang_id")
         diff_id = validated_data.get("diff_id")
         limit = validated_data.get("limit")
-
         ranking_data = self.get_ranking_data(lang_id, diff_id, limit, target_date)
-
         return {
             "status": "success",
             "data": [
                 {
-                    "user_id": data.user.user_id,
-                    "username": data.user.username,
-                    "score": data.score,
+                    "user_id": data["user__user_id"],
+                    "username": data["user__username"],
+                    "score": data["score"],
                 }
                 for data in ranking_data
-                if data.user is not None
             ],
         }
-
     def get_ranking_data(
         self, lang_id: int, diff_id: int, limit: int, target_date: date = None
     ) -> list[Score]:
         """
         ランキングデータを取得します。
         ユーザーごとの最高スコアのみを返します。
-
         Args:
             lang_id (int): 言語ID
             diff_id (int): 難易度ID
@@ -72,11 +64,19 @@ class GetRankingView(BaseView):
         }
         if target_date:
             filter_kwargs["created_at__date"] = target_date
-
         return list(
             Score.objects.filter(**filter_kwargs)
             .select_related("user")
-            .only("user__user_id", "user__username", "score")
-            .order_by("user__user_id", "-score")
-            .distinct("user__user_id")[:limit]
-        )
+            .annotate(
+                rank=Window(
+                    expression=Rank(),
+                    partition_by=[F("user")],
+                    order_by=F("score").desc(),
+                )
+            )
+            .filter(rank=1)
+            .values("user__user_id", "user__username", "score")
+            .distinct()
+            .order_by("-score")
+        )[:limit]
+
